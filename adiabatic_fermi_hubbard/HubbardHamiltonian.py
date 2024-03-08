3864a89eeb4e4aa9b2d887787428925e60722966
Yannick Pleimling <yannickp@vt.edu>
2024-03-07 21:34:35 -0500

    HubbardHamiltonian class documented, initial test set

--- adiabatic_fermi_hubbard/HubbardHamiltonian.py
+++ adiabatic_fermi_hubbard/HubbardHamiltonian.py
@@ -2,7 +2,8 @@ from adiabatic_fermi_hubbard import Lattice
 from qiskit_nature.second_q.operators import FermionicOp
 from qiskit_nature.second_q.mappers import JordanWignerMapper
 
-class HubbardHamiltonian():
+
+class HubbardHamiltonian:
     """A class for constructing the Fermi-Hubbard Hamiltonian for particular lattice, hopping strength, and interaction strength.
 
     Parameters
@@ -14,58 +15,91 @@ class HubbardHamiltonian():
     U : float
         The strength of the onsite interaction term.
     """
-    def __init__(self, lattice:Lattice, t:float=1.0, U:float=1.0):
+
+    def __init__(self, lattice: Lattice, t: float = 1.0, U: float = 1.0):
         self.t = t
         self.U = U
         self.lattice = lattice
 
         # get single fermionic operators
-        n = lattice.get_num_sites() # number of lattice sites
+        n = lattice.get_num_sites()  # number of lattice sites
         i = 0
         creation_operators = []
         annihilation_operators = []
 
-        for i in range(2*n): #make all FermionicOp creation/annihilation operators for 2*n spin orbitals
+        for i in range(
+            2 * n
+        ):  # make all FermionicOp creation/annihilation operators for 2*n spin orbitals
             label = str(i)
-            creation_operators.append(FermionicOp({"+_"+label : 1.0}, num_spin_orbitals=2*n))
-            annihilation_operators.append(FermionicOp({"-_"+label : 1.0}, num_spin_orbitals=2*n))
-        
+            creation_operators.append(
+                FermionicOp({"+_" + label: 1.0}, num_spin_orbitals=2 * n)
+            )
+            annihilation_operators.append(
+                FermionicOp({"-_" + label: 1.0}, num_spin_orbitals=2 * n)
+            )
+
         # build interaction term
         number_operators = []
-        for i in range(0,2*n-1,2):
-            number_operators.append(creation_operators[i] @ annihilation_operators[i] @ creation_operators[i+1] @ annihilation_operators[i+1])     
-        self.interaction_term = U*sum(number_operators)
+        for i in range(0, 2 * n - 1, 2):
+            number_operators.append(
+                creation_operators[i]
+                @ annihilation_operators[i]
+                @ creation_operators[i + 1]
+                @ annihilation_operators[i + 1]
+            )
+        self.interaction_term = U * sum(number_operators)
 
         # build hopping term
         hopping_operators = []
 
         # hopping factors for up-spin fermions (operators with even indices)
-        for i in range(0,2*n-3,2):
-            hopping_operators.append(-1 * creation_operators[i] @ annihilation_operators[i+2] + annihilation_operators[i] @ creation_operators[i+2])
+        for i in range(0, 2 * n - 3, 2):
+            hopping_operators.append(
+                -1 * creation_operators[i] @ annihilation_operators[i + 2]
+                + annihilation_operators[i] @ creation_operators[i + 2]
+            )
 
         # hopping factors for down-spin fermions (operators with odd indices)
-        for i in range(1,2*n-2,2):
-            hopping_operators.append(-1 * creation_operators[i] @ annihilation_operators[i+2] + annihilation_operators[i] @ creation_operators[i+2])
-        
-        # adds hopping between final and initial sites if lattice has pbc
-        if self.lattice.has_pbc():
-            hopping_operators.append(-1 * creation_operators[2*n-2] @ annihilation_operators[0] + annihilation_operators[2*n-2] @ creation_operators[0])
-            hopping_operators.append(-1 * creation_operators[2*n-1] @ annihilation_operators[1] + annihilation_operators[2*n-1] @ creation_operators[1])
-        
-        self.hopping_term = t*sum(hopping_operators)
+        for i in range(1, 2 * n - 2, 2):
+            hopping_operators.append(
+                -1 * creation_operators[i] @ annihilation_operators[i + 2]
+                + annihilation_operators[i] @ creation_operators[i + 2]
+            )
+
+        # adds hopping between final and initial sites if lattice has pbc and has num_sites > 2
+        if self.lattice.has_pbc() and self.lattice.get_num_sites() > 2:
+            hopping_operators.append(
+                -1 * creation_operators[2 * n - 2] @ annihilation_operators[0]
+                + annihilation_operators[2 * n - 2] @ creation_operators[0]
+            )
+            hopping_operators.append(
+                -1 * creation_operators[2 * n - 1] @ annihilation_operators[1]
+                + annihilation_operators[2 * n - 1] @ creation_operators[1]
+            )
+
+        self.hopping_term = t * sum(hopping_operators)
         self.hamiltonian = self.hopping_term + self.interaction_term
 
     def __str__(self):
-        return "t = " + str(self.t) + ", U = " + str(self.U) + "\nLattice:\n" + str(self.lattice) + "\nAs FermionicOp:\n" + str(self.hamiltonian)
-    
+        return (
+            "t = "
+            + str(self.t)
+            + "\nU = "
+            + str(self.U)
+            + "\n\nLattice:\n"
+            + str(self.lattice)
+            + "\n\n"
+            + str(self.hamiltonian)
+        )
+
     def jw_interaction_term(self):
         """Applies Jordan-Wigner transformation to HubbardHamiltonian interaction term and returns result.
 
         Returns
         -------
         jw_interaction : SparsePauliOp
-            The qiskit-nature representation of the Jordan-Wigner transformed interaction term. 
-        
+            The qiskit-nature representation of the Jordan-Wigner transformed interaction term.
+
         """
         jw = JordanWignerMapper()
         jw_interaction = jw.map(self.interaction_term)
@@ -77,8 +111,8 @@ class HubbardHamiltonian():
         Returns
         -------
         jw_hopping : SparsePauliOp
-            The qiskit-nature representation of the Jordan-Wigner transformed hopping term. 
-        
+            The qiskit-nature representation of the Jordan-Wigner transformed hopping term.
+
         """
         jw = JordanWignerMapper()
         jw_hopping = jw.map(self.hopping_term)
@@ -91,15 +125,15 @@ class HubbardHamiltonian():
         -------
         jw_hamiltonian : SparsePauliOp
             The qiskit-nature representation of the Jordan-Wigner transformed Hamiltonian.
-        
+
         """
         jw = JordanWignerMapper()
         jw_hamiltonian = jw.map(self.hamiltonian)
         return jw_hamiltonian
-    
+
     def get_t_value(self):
         """Access method for acquiring the hopping strength of a HubbardHamiltonian.
-        
+
         Returns
         -------
         t : float
@@ -109,17 +143,17 @@ class HubbardHamiltonian():
 
     def get_U_value(self):
         """Access method for acquiring the interaction strength of a HubbardHamiltonian.
-        
+
         Returns
         -------
         U : float
             The value of the interaction strength for the HubbardHamiltonian.
         """
         return self.U
-    
+
     def get_lattice(self):
         """Access method for acquiring the Lattice associated with a HubbardHamiltonian.
-        
+
         Returns
         -------
         lattice : Lattice
@@ -129,7 +163,7 @@ class HubbardHamiltonian():
 
     def get_interaction_term(self):
         """Access method for acquiring the interaction term of a HubbardHamiltonian in FermionicOp form.
-        
+
         Returns
         -------
         interaction_term : FermionicOp
@@ -139,17 +173,17 @@ class HubbardHamiltonian():
 
     def get_hopping_term(self):
         """Access method for acquiring the hopping term of a HubbardHamiltonian in FermionicOp form.
-        
+
         Returns
         -------
         hopping_term : FermionicOp
             The FermionicOp representation of the hopping term.
         """
         return self.hopping_term
-    
+
     def get_hamiltonian(self):
         """Access method for acquiring the HubbardHamiltonian in FermionicOp form.
-        
+
         Returns
         -------
         hamiltonian : FermionicOp
---
+++ adiabatic_fermi_hubbard/tests/test_HubbardHamiltonian.py
@@ -0,0 +1,251 @@
+"""Unit tests for HubbardHamiltonian. """
+
+import adiabatic_fermi_hubbard as afh
+from qiskit_nature.second_q.operators import FermionicOp
+from qiskit_nature.second_q.mappers import JordanWignerMapper
+from qiskit_nature.second_q.hamiltonians import FermiHubbardModel
+from qiskit.quantum_info import SparsePauliOp
+
+
+def test_str():
+    """Test for __str__ method"""
+    lattice1 = afh.Lattice(2, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    test_string = (
+        "t = 2\nU = 2\n\nLattice:\n"
+        + str(lattice1)
+        + "\n\n"
+        + str(ham1.get_hamiltonian())
+    )
+    assert ham1.__str__() == test_string
+
+
+def test_jw_interaction_term():
+    """Test for jw_interaction_Term method"""
+    lattice1 = afh.Lattice(2, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    test_pauli_op = SparsePauliOp(
+        ["IIII", "IIZI", "IIIZ", "IIZZ", "ZIII", "IZII", "ZZII"],
+        coeffs=[
+            1.0 + 0.0j,
+            -0.5 + 0.0j,
+            -0.5 + 0.0j,
+            0.5 + 0.0j,
+            -0.5 + 0.0j,
+            -0.5 + 0.0j,
+            0.5 + 0.0j,
+        ],
+    )
+    assert test_pauli_op == ham1.jw_interaction_term()
+
+
+def test_jw_hopping_term():
+    """Test for jw_hopping_term method"""
+    lattice1 = afh.Lattice(2, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    test_pauli_op = SparsePauliOp(
+        ["IYZY", "IXZX", "YZYI", "XZXI"],
+        coeffs=[-1.0 + 0.0j, -1.0 + 0.0j, -1.0 + 0.0j, -1.0 + 0.0j],
+    )
+    assert test_pauli_op == ham1.jw_hopping_term()
+
+
+def test_jw_hamiltonian():
+    """Test for jw_hamiltonian method"""
+    lattice1 = afh.Lattice(2, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    test_pauli_op = SparsePauliOp(
+        [
+            "IYZY",
+            "IXZX",
+            "YZYI",
+            "XZXI",
+            "IIII",
+            "ZIII",
+            "IZII",
+            "ZZII",
+            "IIZI",
+            "IIIZ",
+            "IIZZ",
+        ],
+        coeffs=[
+            -1.0 + 0.0j,
+            -1.0 + 0.0j,
+            -1.0 + 0.0j,
+            -1.0 + 0.0j,
+            1.0 + 0.0j,
+            -0.5 + 0.0j,
+            -0.5 + 0.0j,
+            0.5 + 0.0j,
+            -0.5 + 0.0j,
+            -0.5 + 0.0j,
+            0.5 + 0.0j,
+        ],
+    )
+
+    # second pauli op has rearrangement of last six Pauli strings
+    test_pauli_op_2 = SparsePauliOp(
+        [
+            "IYZY",
+            "IXZX",
+            "YZYI",
+            "XZXI",
+            "IIII",
+            "IIZI",
+            "IIIZ",
+            "IIZZ",
+            "ZIII",
+            "IZII",
+            "ZZII",
+        ],
+        coeffs=[
+            -1.0 + 0.0j,
+            -1.0 + 0.0j,
+            -1.0 + 0.0j,
+            -1.0 + 0.0j,
+            1.0 + 0.0j,
+            -0.5 + 0.0j,
+            -0.5 + 0.0j,
+            0.5 + 0.0j,
+            -0.5 + 0.0j,
+            -0.5 + 0.0j,
+            0.5 + 0.0j,
+        ],
+    )
+    assert test_pauli_op == ham1.jw_hamiltonian() or test_pauli_op_2 == ham1.jw_hamiltonian()
+
+def test_get_t_value():
+    """Test for get_t_value() access method."""
+    lattice1 = afh.Lattice(4, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    assert ham1.get_t_value() == 2.0
+
+
+def test_get_U_value():
+    """Test for get_U_value() access method."""
+    lattice1 = afh.Lattice(4, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    assert ham1.get_U_value() == 2.0
+
+
+def test_get_lattice():
+    """Test for get_lattice() access method."""
+    lattice1 = afh.Lattice(4, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    lattice_from_ham = ham1.get_lattice()
+
+    assert type(lattice1) == type(lattice_from_ham)
+    assert lattice1.get_num_sites() == lattice_from_ham.get_num_sites()
+    assert lattice1.has_pbc() == lattice_from_ham.has_pbc()
+    assert type(lattice1.get_qiskit_object()) == type(
+        lattice_from_ham.get_qiskit_object()
+    )
+
+
+def test_get_interaction_term():
+    """Test for get_interaction_term() method."""
+    lattice1 = afh.Lattice(2, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    interaction_ham1 = ham1.get_interaction_term()
+
+    create_0 = FermionicOp({"+_0": 1.0}, num_spin_orbitals=4)
+    create_1 = FermionicOp({"+_1": 1.0}, num_spin_orbitals=4)
+    create_2 = FermionicOp({"+_2": 1.0}, num_spin_orbitals=4)
+    create_3 = FermionicOp({"+_3": 1.0}, num_spin_orbitals=4)
+
+    anni_0 = FermionicOp({"-_0": 1.0}, num_spin_orbitals=4)
+    anni_1 = FermionicOp({"-_1": 1.0}, num_spin_orbitals=4)
+    anni_2 = FermionicOp({"-_2": 1.0}, num_spin_orbitals=4)
+    anni_3 = FermionicOp({"-_3": 1.0}, num_spin_orbitals=4)
+
+    interaction_test = 2.0 * (
+        create_0 @ anni_0 @ create_1 @ anni_1 + create_2 @ anni_2 @ create_3 @ anni_3
+    )
+
+    assert interaction_ham1 == interaction_test
+
+
+def test_get_hopping_term():
+    """Test for get_hopping_term() method."""
+    lattice1 = afh.Lattice(2, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    hopping_ham1 = ham1.get_hopping_term()
+
+    create_0 = FermionicOp({"+_0": 1.0}, num_spin_orbitals=4)
+    create_1 = FermionicOp({"+_1": 1.0}, num_spin_orbitals=4)
+    create_2 = FermionicOp({"+_2": 1.0}, num_spin_orbitals=4)
+    create_3 = FermionicOp({"+_3": 1.0}, num_spin_orbitals=4)
+
+    anni_0 = FermionicOp({"-_0": 1.0}, num_spin_orbitals=4)
+    anni_1 = FermionicOp({"-_1": 1.0}, num_spin_orbitals=4)
+    anni_2 = FermionicOp({"-_2": 1.0}, num_spin_orbitals=4)
+    anni_3 = FermionicOp({"-_3": 1.0}, num_spin_orbitals=4)
+
+    hopping_test = 2.0 * (
+        (-1 * (create_0 @ anni_2) + anni_0 @ create_2)
+        + (-1 * (create_1 @ anni_3) + anni_1 @ create_3)
+    )
+
+    lattice2 = afh.Lattice(3, 1)
+    ham2 = afh.HubbardHamiltonian(lattice2, 2, 2)
+    hopping_ham2 = ham2.get_hopping_term()
+
+    assert hopping_ham1 == hopping_test
+
+    create_0 = FermionicOp({"+_0": 1.0}, num_spin_orbitals=6)
+    create_1 = FermionicOp({"+_1": 1.0}, num_spin_orbitals=6)
+    create_2 = FermionicOp({"+_2": 1.0}, num_spin_orbitals=6)
+    create_3 = FermionicOp({"+_3": 1.0}, num_spin_orbitals=6)
+    create_4 = FermionicOp({"+_4": 1.0}, num_spin_orbitals=6)
+    create_5 = FermionicOp({"+_5": 1.0}, num_spin_orbitals=6)
+
+    anni_0 = FermionicOp({"-_0": 1.0}, num_spin_orbitals=6)
+    anni_1 = FermionicOp({"-_1": 1.0}, num_spin_orbitals=6)
+    anni_2 = FermionicOp({"-_2": 1.0}, num_spin_orbitals=6)
+    anni_3 = FermionicOp({"-_3": 1.0}, num_spin_orbitals=6)
+    anni_4 = FermionicOp({"-_4": 1.0}, num_spin_orbitals=6)
+    anni_5 = FermionicOp({"-_5": 1.0}, num_spin_orbitals=6)
+
+    hopping_test_2 = 2.0 * (
+        (-1 * (create_0 @ anni_2) + anni_0 @ create_2)
+        + (-1 * (create_2 @ anni_4) + anni_2 @ create_4)
+        + (-1 * (create_1 @ anni_3) + anni_1 @ create_3)
+        + (-1 * (create_3 @ anni_5) + anni_3 @ create_5)
+        + (-1 * (create_4 @ anni_0) + anni_4 @ create_0)
+        + (-1 * (create_5 @ anni_1) + anni_5 @ create_1)
+    )
+
+    assert hopping_ham2 == hopping_test_2
+
+
+def test_get_hamiltonian():
+    """Test for get_hamiltonian() method."""
+    lattice1 = afh.Lattice(2, 1)
+    ham1 = afh.HubbardHamiltonian(lattice1, 2, 2)
+    assert ham1.get_hopping_term() + ham1.get_interaction_term() == ham1.get_hamiltonian()
+
+#def test_jw_hamiltonian_2():
+#    lattice1 = afh.Lattice(2,1)
+#    ham1 = afh.HubbardHamiltonian(lattice1,2,2)
+#    qiskit_lattice = lattice1.get_qiskit_object()
+#    
+#    fermi_hubbard_test = FermiHubbardModel(
+#    qiskit_lattice.uniform_parameters(
+#        uniform_interaction=-2.0,
+#        uniform_onsite_potential = 0.0
+#    ),
+#    onsite_interaction=2.0
+#    )
+#
+#    create_0 = FermionicOp({"+_0": 1.0}, num_spin_orbitals=4)
+#    create_1 = FermionicOp({"+_1": 1.0}, num_spin_orbitals=4)
+#    create_2 = FermionicOp({"+_2": 1.0}, num_spin_orbitals=4)
+#    create_3 = FermionicOp({"+_3": 1.0}, num_spin_orbitals=4)
+#
+#    anni_0 = FermionicOp({"-_0": 1.0}, num_spin_orbitals=4)
+#    anni_1 = FermionicOp({"-_1": 1.0}, num_spin_orbitals=4)
+#    anni_2 = FermionicOp({"-_2": 1.0}, num_spin_orbitals=4)
+#    anni_3 = FermionicOp({"-_3": 1.0}, num_spin_orbitals=4)
+#
+#    copy = fermi_hubbard_test.second_q_op()
+#
