da369859684a000d2903e4de8621d4db2c9d6a9e
Yannick Pleimling <yannickp@vt.edu>
2024-03-23 17:13:15 -0400

    first version of circuit builder and made pauli rotation test

--- adiabatic_fermi_hubbard/AdiabaticCircuit.py
+++ adiabatic_fermi_hubbard/AdiabaticCircuit.py
@@ -11,7 +11,7 @@ import qiskit as qi
 class AdiabaticCircuit():
     """A class for building/executing Qiskit circuits for adiabatic state preparation of a Hubbard Hamiltonian's ground state"""
     def __init__(self,ham:HubbardHamiltonian,time_step:float=0.1,step_count:int=10):
-        self.hamiltonian = ham
+        self.hubbard_hamiltonian = ham
         self.n = 2 * ham.get_lattice().get_num_sites()
         self.time_step = time_step
         self.step_count = step_count
@@ -23,8 +23,6 @@ class AdiabaticCircuit():
             gates_list.append(pauli_string[i])
         
         #initial step - convert Y, X gates
-        print(gates_list)
-        print(gates_list[0])
         for i in range(len(gates_list)):
             if str(gates_list[i]) == "X":
                 circ.h(i)
@@ -48,10 +46,52 @@ class AdiabaticCircuit():
                 circ.rx(-3*np.pi/2, i)
         
         return circ
-                    
+
+    def evolution_operator(self,k):
+        delta_s = 1/self.step_count
+        
+        circ = qi.QuantumCircuit(self.n,0)
+
+        final_ham = self.hubbard_hamiltonian.jw_hamiltonian()
+        final_ham_paulis = final_ham.paulis
+        final_ham_coeffs = final_ham.coeffs
+        final_ham_terms = []
+
+        for i in range(len(final_ham_paulis)):
+            final_ham_terms.append(final_ham_paulis[i])
+
+        seq = []
+        for i in range(self.n):
+            seq.append(i)
+
+        for i in range(self.n):
+            circ.rx(self.time_step*(1-k*delta_s), i)
+    
+        for i in range(len(final_ham_paulis)):
+            circ.append(self.pauli_string_rotation(final_ham_terms[i],self.time_step*k*delta_s*np.real(final_ham_coeffs[i])),seq)
+        
+        
+        return circ
+       
 
     def get_circuit(self):
-        pass
+        circ = qi.QuantumCircuit(self.n,0)
+        for i in range(self.n):
+            circ.x(i)
+            circ.h(i)
+        
+        seq = []
+        for i in range(self.n):
+            seq.append(i)
+
+        for i in range(self.step_count+1):
+            circ.append(self.evolution_operator(i),seq)
+        
+        return circ
+
 
     def get_num_qubits(self):
         return self.n
+    
+    def get_hubbard_hamiltonian(self):
+        return self.hubbard_hamiltonian
--- adiabatic_fermi_hubbard/tests/test_AdiabaticCircuit.py
+++ adiabatic_fermi_hubbard/tests/test_AdiabaticCircuit.py
@@ -1,1 +1,31 @@
+import adiabatic_fermi_hubbard as afh
 import qiskit as qi
+import numpy as np
+
+def test_pauli_string_rotation():
+    '''Test method for pauli_string_rotation function'''
+    #do XYIZ rotation, phase pi
+    circ = qi.QuantumCircuit(4,0)
+    circ.h(0)
+    circ.rx(3*np.pi/2,1)
+    circ.cx(0,3)
+    circ.cx(1,3)
+    circ.cx(2,3)
+    circ.rz(np.pi, 3)
+    circ.cx(2,3)
+    circ.cx(1,3)
+    circ.cx(0,3)
+    circ.rx(-3*np.pi/2,1)
+    circ.h(0)
+
+    circ_drawn = circ.draw(output="text")
+
+    #use method
+    lattice1 = afh.Lattice(2,1)
+    ham1 = afh.HubbardHamiltonian(lattice1)
+    ad_circ = afh.AdiabaticCircuit(ham1)
+    paulis = qi.quantum_info.Pauli("XYIZ")
+    circ1 = ad_circ.pauli_string_rotation(paulis,np.pi)
+    circ1_drawn = circ1.draw(output="text")
+
+    assert str(circ) == str(circ1)
